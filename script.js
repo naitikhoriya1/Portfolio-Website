315759261cda3255a8eea5444430a4f42f1d3376
Naitik khoriya <naitikkhoriya144263@gmail.com>
2025-01-25 20:33:03 +0530

    `Update opacity and end trigger of scrollTrigger in script.js`

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 document.querySelectorAll(".card").forEach((card) => {
   gsap.to(card, {
     scale: 0.7,
-    opacity: 0,
+    opacity: 0.3,
     scrollTrigger: {
       trigger: card,
       start: "top 15%",
-      end: "bottom 15%",
+      end: "bottom bottom",
       // markers: true, // Uncomment this to see markers for debugging
       scrub: true,
     },
